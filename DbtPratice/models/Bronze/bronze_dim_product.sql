38117f479bd09d5a77be03f3bdd174f44e5ec1e9
Milan Paudel <milanpaudel784@gmail.com>
2026-06-06 21:03:12 +0545

    before understanding dbt test

--- DbtPratice/models/Bronze/bronze_dim_product.sql
+++ DbtPratice/models/Bronze/bronze_dim_product.sql
@@ -1,11 +1,5 @@
 {{ config(
-    materialized='view',
-    -- sql_header="<string>"
-    -- on_configuration_change: apply | continue | fail # only for materialized views for supported adapters
-    -- unique_key='column_name_or_expression'
-    -- freshness=<dict>
-    -- on_error="skip_children" | "continue"
-    -- latest_version_pointer=<dict>
+    materialized='view'
 ) }}
 
 select
